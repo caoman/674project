515565234f076074bc9a6747d59e6ba711e7c352
caoman <tsaoman@gmail.com>
2013-09-04 21:18:56 -0400

    test

--- preprocess.py
+++ preprocess.py
@@ -4,13 +4,20 @@ from bs4 import BeautifulSoup
 
 
 
-
 def processFile(filename):
+	infile = open(filename, 'r')
+	outfile = open('out'+filename+'.txt', 'w')
 	soup = BeautifulSoup(infile)
 	#outfile.print(soup.title)
 	#titles = soup.find_all('title')
 	entries = soup.find_all('reuters')
 
-for t in entries:
-	print t.get('newid')
+	for t in entries:
+		outfile.writelines(t.get('newid'))
 
+	infile.close()
+	outfile.close()
+
+
+
+processFile('reut2-000.sgm')
