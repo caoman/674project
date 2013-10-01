2ecf829df85d0bb7a69bb3f30a15144fbcde3601
caoman <tsaoman@gmail.com>
2013-09-30 22:24:06 -0400

    updated code

--- project2/main2.py
+++ project2/main2.py
@@ -4,38 +4,38 @@ Created on Sep 18, 2013
 
 @author: lilong, man
 '''
-import re, os, math, random, heapq, operator
+import re, os, math, random, heapq, operator, time
 
 docList = []
 trainDocList = []
 testDocList = []
-classDict = {}
 valFold = 5     #4:1 cross validation
 numTopics = 2   #Number of topics per doc predicted by classification algorithm. If it's 1, the effect is equivalent to the old code.
-#bayesM = 3      # parameters of "m-estimate" for naive Bayes. Set bayesM to zero to disable m-estimate.
-bayesM = 0
-bayesP = 0.333
+allWords = set()
 
 
 class Doc:
     def __init__(self, topics, feaVec):
         self.topics = topics
         self.feaVec = feaVec
+        for w in feaVec.iterkeys():
+            allWords.add(w)
 
 class TopicClass:
     def __init__(self, doc, topic):
         self.docList = []
         self.topic = topic
         self.wordCnt = 0
-        self.vector = {}
+        # mapping of word : docs that contain the word
+        self.wordDocVec = {}
         self.addDoc(doc)
     
     def addDoc(self, doc):
         self.docList.append(doc)
         for token, val in doc.feaVec.iteritems():
             self.wordCnt += val
-            if self.vector.has_key(token): self.vector[token] += val
-            else: self.vector[token] = val
+            if self.wordDocVec.has_key(token): self.wordDocVec[token] += 1
+            else: self.wordDocVec[token] = 1
 
 
 #tested
@@ -126,18 +126,7 @@ def classifyWithKNN():
                     topicDict[topic] += 1
                 else:
                     topicDict[topic] = 1
-                
-        '''
-        docTopic = ""                       #the topic with the maximum freq in the neighbors
-        freq = 0
-        #print "distinct topics: " + str(len(topicDict))
-        for topic, val in topicDict.items():
-            if val > freq:
-                freq = val
-                docTopic = topic
-        if docTopic in testDoc.topics:
-            relCnt += 1
-        '''
+        
         if isRelated(testDoc, topicDict):
             relCnt += 1
     precision = float(relCnt) / testDocSize
@@ -146,62 +135,62 @@ def classifyWithKNN():
 #Not consider the count of the words, prob(w | Ci) = countDocs(contains(w), Ci) / totalDocs(Ci)
 def classifyWithBayes():
     global trainDocList, testDocList
-    global classDict
     
-    mulTopicsDocList = []
+    bayesM = len(trainDocList)
+    # mapping of word : num of docs that contain the word
+    wordDocFreq = {}
     classDict = {}
     
+    startTime = time.time()
     docCnt = 0
     for doc in trainDocList:
-        '''
-        if len(doc.topics) > 1: 
-            mulTopicsDocList.append(doc)
-            continue
-        docTopic = doc.topics[0]
-        '''
         for docTopic in doc.topics:
             docCnt += 1     # Count the doc for multiple times, if it has multiple topics
             if classDict.has_key(docTopic):
                 classDict[docTopic].addDoc(doc)
             else:
                 classDict[docTopic] = TopicClass(doc, docTopic)
-    ''' 
-    for doc in mulTopicsDocList:
-        for topic, topicClass in classDict.iteritems():
-            if topic in doc.topics:
-                topicClass.addDoc(doc)
-    '''
+    
+        for w in doc.feaVec.iterkeys():
+            if wordDocFreq.has_key(w): wordDocFreq[w] += 1
+            else: wordDocFreq[w] = 1
+
+    endTime = time.time()
+    print "Bayes Training time: " + str(endTime - startTime)
     topicDict = {}
     relCnt = 0
+    
+    startTime = time.time()
     for doc in testDocList:
         for topicClass in classDict.itervalues():
             priorProb = float(len(topicClass.docList)) / docCnt
             itemsProb = 1
             for token in doc.feaVec.keys():
-                itemOcc = 0 + bayesM * bayesP
-                for classDoc in topicClass.docList:
-                    if classDoc.feaVec.has_key(token):
-                        itemOcc += 1
+                if wordDocFreq.has_key(token):
+                    bayesP = float(wordDocFreq[token]) / len(trainDocList)
+                else:
+                    bayesP = 0.01 / len(trainDocList)
+                itemOcc = bayesM * bayesP
+                if topicClass.wordDocVec.has_key(token):
+                    itemOcc += topicClass.wordDocVec[token]
                 itemsProb *= float(itemOcc) / (len(topicClass.docList) + bayesM)
             tmpProb = itemsProb * priorProb
             topicDict[topicClass.topic] = tmpProb
-            '''
-            if tmpProb > prob:
-                prob = tmpProb
-                docClass = topicClass.topic
-            '''
-        #if docClass in doc.topics:
         if isRelated(doc, topicDict):
             relCnt += 1
     
+    endTime = time.time()
+
+    print "Bayes Testing time: " + str((endTime - startTime)/float(len(testDocList)))
     precision = float(relCnt) / len(testDocList)
     print "Bayes precision:" + str(precision)
 
+'''
 # Consider the count of each word, prob(w | Ci) = count(w in Ci) / totalWords(Ci)
 def classifyWithBayes2():
     global trainDocList, testDocList
-    global classDict
     
+    bayesM = len(trainDocList)
     classDict = {}
     
     docCnt = 0
@@ -233,7 +222,7 @@ def classifyWithBayes2():
     
     precision = float(relCnt) / len(testDocList)
     print "Bayes precision:" + str(precision)
-
+'''
 def crossValidate():
     global testDocList
     global trainDocList
@@ -253,26 +242,10 @@ def crossValidate():
             testDocList = docList[startIndex:] + docList[0: endIndex]
             trainDocList = docList[endIndex: startIndex]
             
-        #printTrainTest()
         #classifyWithKNN()
-        #classifyWithBayes()
-        classifyWithBayes2()
-        #break
+        classifyWithBayes()
+        #classifyWithBayes2()
         
-        
-def printTrainTest():
-    global trainDocList
-    global testDocList
-    print "--------------------------------"
-    print "trainDocList:" + str(len(trainDocList))
-    for doc in trainDocList:
-        print doc.topics 
-        print doc.feaVec
-    print "testDocList:" + str(len(testDocList))
-    for doc in testDocList:
-        print doc.topics 
-        print doc.feaVec
-  
 if __name__ == '__main__':
     readVectors("FreqVectors.txt")
     crossValidate() 
