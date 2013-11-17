6f448e121d2c2aff6c7056074a39683af598130e
Lilong <jianglilong0225@gmail.com>
2013-11-17 15:46:13 -0500

    update project4

---
+++ project4/AssociationRule1.py
@@ -0,0 +1,45 @@
+'''
+Created on Nov 17, 2013
+
+@author: lilong
+'''
+import Orange
+
+topicSet = set()
+transFileName = "Transactions.basket"
+freqFileName = "FreqVectors.txt"
+valFold = 5
+
+#Transform vector from the key-value to transaction form
+def transformVec():
+    global topicSet
+    freqFile = open(freqFileName, 'r')
+    transFile = open(transFileName, 'w')
+    
+    while True:
+        metaData = freqFile.readline() 
+        if len(metaData) == 0: break
+        metaVec = eval(metaData)
+        freqVec = eval(freqFile.readline())    
+        transFile.write(",".join(freqVec.keys() + metaVec["TOPICS"]) + "\n")
+        for topic in metaVec["TOPICS"]:
+            topicSet.add(topic)
+    freqFile.close()
+    transFile.close()        
+
+def getAssociationRules():
+    data = Orange.data.Table(transFileName)
+
+    #Cross Validation
+    cvIndices = Orange.data.sample.SubsetIndicesCV(data, valFold)
+    for fold in range(valFold):
+        train = data.select(cvIndices, fold, negate = 1)
+        test  = data.select(cvIndices, fold)
+        rules = Orange.associate.AssociationRulesSparseInducer(train, support = 0.3)   
+        for rule in rules:
+            
+
+if __name__ == '__main__':
+    transformVec()
+    #getAssociationRules()
+    
